32bce96ec6893444e10aae0e4931b551ec85bde9
midia-lima <midialp2@gmail.com>
2021-04-06 20:48:29 -0300

    validaçoes e correção da API

--- js/main.js
+++ js/main.js
@@ -1,24 +1,34 @@
-const input = document.querySelector('input[name="cep"]');
+var input = document.querySelector('input[name="cep"]');
 const button = document.querySelector('#btn');
 const result = document.querySelector('.infos');
 
+function limpaCampo(){
+    document.getElementById('input').value=("");
+}
+
 button.addEventListener('click', async event => {
+    event.preventDefault();     
+   
+    if(input.value == ""){
+        alert("Digite o CEP")        
+        return
+    }
+    if(input.value == "00000000" || input.value == "12345678"){
+        alert("Formato de CEP inválido")     
+        limpaCampo()  
+        return
+    } else {
 
-    event.preventDefault();
+        const response = await fetch(`https://viacep.com.br/ws/${input.value}/json/`);    
+        const{localidade, uf, bairro, logradouro} = await response.json();    
+        result.innerHTML = `
+        <p>Logradouro: ${logradouro}</p>
+        <p>Bairro: ${bairro}</p>
+        <p>Cidade: ${localidade}</p>
+        <p>Estado: ${uf}</p>     
+        `
+        document.querySelector('#load'). innerHTML = ''
 
-    document.querySelector('#load').innerHTML = 'Buscando...'
-
-    const response = await fetch(`https://cleantech.ga/cep/${input.value}`);       
-
-    const{cidade, estado, bairro, logradouro} = await response.json();    
-
-    result.innerHTML = `
-    <p>Logradouro: ${logradouro}</p>
-    <p>Bairro: ${bairro}</p>
-    <p>Cidade: ${cidade}</p>
-    <p>Estado: ${estado}</p>     
-    `
-
-    document.querySelector('#load'). innerHTML = ''    
+    }       
     
 })
