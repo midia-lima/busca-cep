e6c0e8de8e6ec9e15432ad2e8adc22cca48cd1db
midia-lima <midialp2@gmail.com>
2021-04-06 21:20:10 -0300

    mascara de CEP

--- js/main.js
+++ js/main.js
@@ -1,34 +1,49 @@
 var input = document.querySelector('input[name="cep"]');
 const button = document.querySelector('#btn');
 const result = document.querySelector('.infos');
+$("#input").mask("00000-000");
 
-function limpaCampo(){
-    document.getElementById('input').value=("");
+function limpaCampo() {
+    document.getElementById('input').value = ("");
+}
+
+function limpaRetornoEndereco() {
+    result.innerHTML = `
+    <p></p>
+    <p></p>
+    <p></p>
+    <p></p>     
+    `
 }
 
 button.addEventListener('click', async event => {
-    event.preventDefault();     
-   
-    if(input.value == ""){
-        alert("Digite o CEP")        
+    event.preventDefault();
+
+    console.log(input.value);
+
+    if (input.value == "") {
+        alert("Digite o CEP")
+        limpaRetornoEndereco()
         return
     }
-    if(input.value == "00000000" || input.value == "12345678"){
-        alert("Formato de CEP inválido")     
-        limpaCampo()  
+    if (input.value == "00000-000" || input.value == "12345-678") {
+        alert("Formato de CEP inválido")
+        limpaCampo()
+        limpaRetornoEndereco()
         return
     } else {
 
-        const response = await fetch(`https://viacep.com.br/ws/${input.value}/json/`);    
-        const{localidade, uf, bairro, logradouro} = await response.json();    
+        const response = await fetch(`https://viacep.com.br/ws/${input.value}/json/`);
+        const { localidade, uf, bairro, logradouro } = await response.json();
         result.innerHTML = `
         <p>Logradouro: ${logradouro}</p>
         <p>Bairro: ${bairro}</p>
         <p>Cidade: ${localidade}</p>
         <p>Estado: ${uf}</p>     
         `
-        document.querySelector('#load'). innerHTML = ''
+        document.querySelector('#load').innerHTML = ''
+        limpaCampo()
 
-    }       
-    
+    }
+
 })
